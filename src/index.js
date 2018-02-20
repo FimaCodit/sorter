706bb606bfd2cd0c5ee560b542bf6bd66473f8ad
Efim Poleschuk <limitlessFIMA@gmail.com>
2018-02-20 23:26:33 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 class Sorter {
   constructor() {
     this.arr = [];
+    this.temp = [];
   }
 
   add(element) {
-   this.arr.push(element);
-  
+    this.arr.push(element);
   }
 
   at(index) {
-    return this.arr[i];
+    return this.arr[index];
   }
 
   get length() {
@@ -21,16 +21,20 @@ class Sorter {
   }
 
   sort(indices) {
-    indices = indices.sort(function(a,b){
+    for (let i = 0; i < indices.length; i++) {
+      this.temp.push(this.arr[indices[i]]);
+    }
+    this.temp.sort(function(a,b){
       return a - b;
     })
+    for (let i = 0; i < indices.length; i++) {
+      this.arr[indices[i]] = this.temp[i];
+    }  
   }
 
   setComparator(compareFunction) {
-    // your implementation
+    return this.arr.sort(compareFunction);
   }
 }
-
-
 
 module.exports = Sorter;
